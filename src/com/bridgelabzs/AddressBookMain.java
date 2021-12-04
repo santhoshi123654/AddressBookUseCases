aed14fb9d2c81735efd7a052666fe879253a8ad1
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-12-04 18:24:26 +0530

    Added:AddressBook Adding ,Editing and Deleting

--- src/com/bridgelabzs/AddressBookMain.java
+++ src/com/bridgelabzs/AddressBookMain.java
@@ -5,51 +5,40 @@ import java.util.Scanner;
 public class AddressBookMain {
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		AddressBook ab = new AddressBook();
-		int choice;
-		char c;
-		System.out.println("Welcome to AddressBook");
 
-		do {
-			System.out.println("Enter \n1.Add details \n2.Update details \n3.Delete details "
-					+ "\n4.Show members \n5.No of persons \n6.Print details");
-			choice = sc.nextInt();
-			switch (choice) {
+		System.out.println("Welcome To Address Book");
+		AddressBook addressBook = new AddressBook(null, null, null, null, null, null, null,null);
+		Contact contact = new Contact();
+		Scanner scanner = new Scanner(System.in);
+		int exit = 0;
+		
+		while (exit != 4) {
+			
+			System.out.println("\n Enter 1:To Add \n Enter 2 :TO Edit \n Enter 3 :To Delete \n Enter 4: Exit ");
+			int num = scanner.nextInt();
+			exit = num;
+			
+			switch (num) {
+			
 				case 1:
-					ab.addPerson();
+					addressBook = Contact.contact();
+					contact.addContact(addressBook);
 					break;
 				case 2:
-					System.out.println("Enter your first name to edit person details:");
-					String fName = sc.next();
-					int i = ab.getIndex(fName);
-					ab.editContact(i);
+					System.out.println("Enter the first name to Edit");
+					String name = scanner.next();
+					contact.editDetails(name, addressBook);
 					break;
 				case 3:
-					System.out.println("Enter your first name to delete person details:");
-					String fNameDelete = sc.next();
-					int delete = ab.getIndex(fNameDelete);
-					ab.deleteContact(delete);
+					System.out.println("Enter FirstName to Delete ");
+					String deletingname = scanner.next();
+					contact.deleteContact(deletingname, addressBook);
 					break;
-				case 4:
-					ab.display();
+				case 4 :
+					System.out.println("Exit from the AddresBook");
+					System.out.println(contact.addresses);
 					break;
-				case 5:
-					System.out.println("No of person details in your AddressBook:" + new Contact().members);
-					System.exit(0);		
-					break;
-				case 6:
-					System.out.println("Print Contact Details");
-					 ab.printContact();
-					 break;
 			}
-			
-			System.out.println("Do you Want to continue(Y/N):");
-			c = sc.next().charAt(0);
-			
 		}
-		while(c == 'Y' || c == 'y' );
-		sc.close();
-		
 	}
 }
