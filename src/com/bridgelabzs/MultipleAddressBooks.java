34c25c9b9936079cc216eb6fb5caefef8c782ee8
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-12-04 20:44:54 +0530

    Added:MultipleAddressBook

---
+++ src/com/bridgelabzs/MultipleAddressBooks.java
@@ -0,0 +1,15 @@
+package com.bridgelabzs;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MultipleAddressBooks {
+	
+	Map<String, List> mapBook = new HashMap<>();
+
+	@Override
+	public String toString() {
+		return "MultipleAddressBooks [mapBook=" + mapBook + "]";
+	}
+}
