0fbbe9b4cf36a9eb1b151ab1e89a75b010e99a92
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-12-04 16:23:39 +0530

    Added:Edit Contact details Program

--- src/com/bridgelabzs/AddressBook.java
+++ src/com/bridgelabzs/AddressBook.java
@@ -14,11 +14,10 @@ public class AddressBook {
 	String zipCode;
 	String phoneNumber;
 	String emailId;
-	int members = 0;
 
-	public AddressBook(String firstName, String lastName, String address, String cityName, String stateName, String zipCode,
-			String phoneNumber, String emailId) {
-
+	public AddressBook(String firstName, String lastName, String address, String cityName, String stateName,
+			String zipCode, String phoneNumber, String emailId) {
+		
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -28,6 +27,78 @@ public class AddressBook {
 		this.zipCode = zipCode;
 		this.phoneNumber = phoneNumber;
 		this.emailId = emailId;
-		members++;
 	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public String getStateName() {
+		return stateName;
+	}
+
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	@Override
+	public String toString() {
+		return "AddressBook [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", cityName="
+				+ cityName + ", stateName=" + stateName + ", zipCode=" + zipCode + ", phoneNumber=" + phoneNumber
+				+ ", emailId=" + emailId + "]";
+	}
+	
+	
 }
--- src/com/bridgelabzs/Contact.java
+++ src/com/bridgelabzs/Contact.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 
 public class Contact {
 	
-	public static void main(String[] args) {
-
+	public static AddressBook contact() {
+		
 		System.out.println("Welcome to Address Book");
 		Scanner sc = new Scanner(System.in);
 		System.out.print("First Name:");
@@ -15,74 +15,39 @@ public class Contact {
 		System.out.print("Last Name:");
 		String lName = sc.nextLine();
 		System.out.print("Enter Address:");
-		String add = sc.nextLine();
+		String address = sc.nextLine();
 		System.out.print("Enter City:");
-		String c = sc.nextLine();
+		String cityName = sc.nextLine();
 		System.out.print("Enter State:");
-		String s = sc.nextLine();
+		String stateName = sc.nextLine();
 		System.out.print("Enter ZIP:");
-		String p = sc.nextLine();
+		String zipCode = sc.nextLine();
 		System.out.print("Enter PhoneNumber:");
-		String pN = sc.nextLine();
+		String phoneNumber = sc.nextLine();
 		System.out.print("Enter email id:");
-		String eid = sc.nextLine();
-
-		AddressBook member1 = new AddressBook(fName, lName, add, c, s, p, pN, eid);
-
-		// creating array list
-
-		ArrayList<AddressBook> al = new ArrayList<AddressBook>();
-		al.add(member1);
-
-		// Getting Iterator
-
-		Iterator itr = al.iterator();
-
-		// traversing elements of Array List Object
-
-		while (itr.hasNext()) {
-			AddressBook ab = (AddressBook) itr.next();
-			System.out.println(ab.firstName + "" + ab.lastName + "\n" + ab.address + "\n" + ab.cityName + "\n" + ab.stateName
-					+ "\n" + ab.zipCode + "\n" + ab.phoneNumber + "\n" + ab.emailId + "\n");
+		String emailId = sc.nextLine();
+		
+		AddressBook addressBook = new AddressBook(fName, lName, address,cityName,stateName,zipCode,phoneNumber,emailId);
+		return addressBook;
+}
+	public AddressBook editDetails(String name, AddressBook addressBook) {
+		
+		if (addressBook.getFirstName().equals(name)) {
+			addressBook = contact();
 		}
+		return addressBook;
+	}
 
-		System.out.println("Enter the contact Name to edit:");
-		String name = sc.nextLine();
-		for (AddressBook contact : al) {
-			if (contact.firstName.equals(name)){
-				
-				System.out.println("Edit the contact details");
-				Scanner sc1 = new Scanner(System.in);
-				System.out.print("First Name:");
-				String fName1 = sc.nextLine();
-				System.out.print("Last Name:");
-				String lName1 = sc.nextLine();
-				System.out.print("Enter Address:");
-				String add1 = sc.nextLine();
-				System.out.print("Enter City:");
-				String c1 = sc.nextLine();
-				System.out.print("Enter State:");
-				String s1 = sc.nextLine();
-				System.out.print("Enter ZIP:");
-				String p1 = sc.nextLine();
-				System.out.print("Enter PhoneNumber:");
-				String pN1 = sc.nextLine();
-				System.out.print("Enter email id:");
-				String eid1 = sc.nextLine();
-				AddressBook member2 = new AddressBook(fName1, lName1, add1, c1, s1, p1, pN1, eid1);
-				
-				al.set(0, member2);
-			}
-			
-			System.out.println("\nEdited contact details\n");
-			
-			while (itr.hasNext()) {
-				AddressBook ab = (AddressBook) itr.next();
-				System.out.println("\n............................................\n");
-				System.out.println(ab.firstName + "" + ab.lastName + "\n" + ab.address + "\n" + ab.cityName + "\n" + ab.stateName
-						+ "\n" + ab.zipCode + "\n" + ab.phoneNumber + "\n" + ab.emailId + "\n");
-			}
-		}
+	public static void main(String[] args) {
 
+		AddressBook addressBook = contact();
+		System.out.println(addressBook);
+		System.out.println();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the Name to be Edited :");
+		String name = scanner.next();
+		Contact contact=new Contact();
+		addressBook =contact.editDetails(name,addressBook);
+		System.out.println(addressBook);
 	}
 }
